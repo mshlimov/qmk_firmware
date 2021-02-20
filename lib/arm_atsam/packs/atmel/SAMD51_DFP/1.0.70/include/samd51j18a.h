34cb843fc07fe5cc97b4d8cb4b1a99dff72300c1
Mark Shlimovich <mark@shlim.net>
2021-02-20 00:13:05 -0800

    Fix double define

--- lib/arm_atsam/packs/atmel/SAMD51_DFP/1.0.70/include/samd51j18a.h
+++ lib/arm_atsam/packs/atmel/SAMD51_DFP/1.0.70/include/samd51j18a.h
@@ -529,6 +529,7 @@ void SDHC0_Handler               ( void );
  * \brief Configuration of the Cortex-M4 Processor and Core Peripherals
  */
 
+#undef LITTLE_ENDIAN
 #define LITTLE_ENDIAN          1        
 #define __CM4_REV              1         /*!< Core revision r0p1 */
 #define __DEBUG_LVL            3         /*!< Full debug plus DWT data matching */
